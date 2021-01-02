c6393d488174b6abf60340c86c0dd50614855c23
Gaganpal Singh <gaganpal.singh@check24.de>
2021-01-02 18:48:27 +0100

    first commit

---
+++ Sample_test.js
@@ -0,0 +1,6 @@
+Feature('Sample Test with Allure and Jenkins');
+
+Scenario('test something', ({ I }) => {
+    I.amOnPage('https://handyversicherung.check24.de/c24-odi-checkin/desktop/results');
+
+});
